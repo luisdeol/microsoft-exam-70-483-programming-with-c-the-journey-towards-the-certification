4a249252d19bbea851e1ba388b50b332a08120a0
luisdeol <luisfelipekai@gmail.com>
2019-03-11 01:34:24 -0300

    [Skill 2] Finishing adding files to the "Consume types" topic.

---
+++ Skill 2 - Create and use types/CreateAndUseTypes/ConsumeTypes/COMApiDynamic.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsumeTypes
+{
+    class COMApiDynamic
+    {
+        //static void Main(string[] args)
+        //{
+
+        //    var students = new List<dynamic>
+        //    {
+        //        new { Name = "Luis", School = "School A" },
+        //        new { Name = "Felipe", School = "School B" },
+        //        new { Name = "Shurato", School = "School C" },
+        //        new { Name = "Bucky", School = "School D" }
+        //    };
+
+        //    ExportStudentDataToExcel(students);
+        //    Console.ReadKey();
+        //}
+
+        //static void ExportStudentDataToExcel (IEnumerable<dynamic> students)
+        //{
+        //    var excelApplication = new Excel.Application();
+        //    excelApplication.Visible = true;
+        //    excelApplication.Workbooks.Add();
+        //    dynamic workSheet = excelApplication.ActiveSheet;
+
+        //    workSheet.Cells[1, "A"] = "Student";
+        //    workSheet.Cells[1, "B"] = "School";
+
+        //    var count = 1;
+        //    foreach (var student in students)
+        //    {
+        //        count++;
+        //        workSheet.Cells[count, "A"] = student.Name;
+        //        workSheet.Cells[count, "B"] = student.School;
+        //    }
+
+        //    workSheet.Columns[1].AutoFit();
+        //    workSheet.Columns[2].AutoFit();
+        //}
+    }
+}
--- Skill 2 - Create and use types/CreateAndUseTypes/ConsumeTypes/HelperClasses.cs
+++ Skill 2 - Create and use types/CreateAndUseTypes/ConsumeTypes/HelperClasses.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsumeTypes
+﻿namespace ConsumeTypes
 {
     class HelperClasses
     {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine($"String converted to int using:");
+
+        //    string aNumber = "1992";
+
+        //    int aNumberParse = int.Parse(aNumber);
+
+        //    // If TryParse returns falses, it means that the conversion is not
+        //    // allowed.
+        //    if (int.TryParse(aNumber, out int numberTryParse))
+        //    {
+        //        Console.WriteLine($"int.TryParse method: {numberTryParse}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("aNumber is not a number!");
+        //        return;
+        //    }
+
+        //    int aNumberConvert = Convert.ToInt32(aNumber);
+
+
+        //    Console.WriteLine($"int.Parse method: {aNumberConvert}");
+        //    Console.WriteLine($"Convert.ToInt32 method: {aNumberConvert}");
+
+
+        //    Console.ReadKey();
+        //}
     }
 }
