b33eeba852f8c76aa703e57072fdb8a624309b37
luisdeol <luisfelipekai@gmail.com>
2019-03-27 08:27:25 -0300

    [EnforcingEncapsulation] Cleaning Code on Program.cs and creating Explicit interface implementation own class file.

---
+++ Skill 2 - Create and use types/CreateAndUseTypes/EnforcingEncapsulation/ExplicitInterfaceImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnforcingEncapsulation
+{
+    class ExplicitInterfaceImpl
+    {
+    }
+
+    //public interface ITruckDriver
+    //{
+    //    void Drive();
+    //}
+
+    //public interface ICarDriver
+    //{
+    //    void Drive();
+    //}
+
+    //public class Driver : ITruckDriver, ICarDriver
+    //{
+    //    void ITruckDriver.Drive()
+    //    {
+    //        Console.WriteLine("Driving the truck!");
+    //    }
+
+    //    void ICarDriver.Drive()
+    //    {
+    //        Console.WriteLine("Driving the car!");
+    //    }
+    //}
+}
