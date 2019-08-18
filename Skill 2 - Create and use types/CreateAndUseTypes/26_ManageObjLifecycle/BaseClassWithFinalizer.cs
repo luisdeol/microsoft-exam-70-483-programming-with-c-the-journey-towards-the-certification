3577ef3bc04556c0ad6ce73eaa4a73e286d51e5c
luisfelipe <luisdeol@hotmail.com>
2019-08-18 15:00:12 -0300

    2.6 - Managing the object lifecycle

---
+++ Skill 2 - Create and use types/CreateAndUseTypes/26_ManageObjLifecycle/BaseClassWithFinalizer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _26_ManageObjLifecycle
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var baseClassWithFinalizer = new BaseClassWithFinalizer();
+    //        baseClassWithFinalizer = null;
+
+    //        GC.Collect();
+    //        GC.WaitForPendingFinalizers();
+
+    //        Console.ReadKey();
+    //    }
+    //}
+
+    public class BaseClassWithFinalizer
+    {
+        private UnmanagedResource _unmanagedResource;
+
+        public BaseClassWithFinalizer()
+        {
+            _unmanagedResource = new UnmanagedResource();
+            _unmanagedResource.DoStuff();
+        }
+
+        ~BaseClassWithFinalizer()
+        {
+            Console.WriteLine("BaseClassWithFinalizer is being called.");
+
+            _unmanagedResource.Free();
+        }
+    }
+
+    public class UnmanagedResource
+    {
+        public void DoStuff()
+        {
+            Console.WriteLine("Doing unmanaged stuff. File access created.");
+        }
+
+        public void Free()
+        {
+            Console.WriteLine("Freeing Unmanaged Resource.");
+        }
+    }
+}
---
+++ Skill 2 - Create and use types/CreateAndUseTypes/26_ManageObjLifecycle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _26_ManageObjLifecycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseClassWithFinalizer = new BaseClassWithFinalizer();
+            baseClassWithFinalizer = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.ReadKey();
+        }
+    }
+}
