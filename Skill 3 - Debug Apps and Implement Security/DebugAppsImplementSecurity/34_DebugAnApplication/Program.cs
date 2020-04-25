7bf9851ecf36630bf05535a91893b2f92e07dbc1
luisfelipe <luisdeol@hotmail.com>
2020-04-24 21:01:44 -0300

    3.4: Debug an Application.

---
+++ Skill 3 - Debug Apps and Implement Security/DebugAppsImplementSecurity/34_DebugAnApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿#define CUSTOM_DIR
+// #undef CUSTOM_DIR 
+// undef removes the preprocessor directive definition. Uncomment the code above and notice how the display message changes.
+
+using System;
+
+namespace _34_DebugAnApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 40;
+            var anotherNumber = 100;
+
+            #if DEBUG
+            Console.WriteLine("Debug mode.");
+            #else
+	        Console.WriteLine("Release mode.");
+#endif
+
+            var sum = number + anotherNumber;
+
+            #if CUSTOM_DIR
+            Console.WriteLine("CUSTOM_DIR defined");
+            #else
+            Console.WriteLine("CUSTOM_DIR NOT DEFINED");
+            #endif
+
+            #region REGION 1
+            Console.WriteLine("Region 1");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Skill 3 - Debug Apps and Implement Security/DebugAppsImplementSecurity/WinRTComponent/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("WinRTComponent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinRTComponent")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão 
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
